e77c1789ec312940509c7edab569ad81baf0fbef
delahozric <delahozric@gmail.com>
2021-02-09 18:01:58 -0500

    js numbers assignment

---
+++ week-3/ricardo_delahoz_js.numbers/script.js
@@ -0,0 +1,9 @@
+var exp = "100 + 50 - 25 + 0 + 123 - 1".split(" ");
+
+// var sum = 0;
+console.log("Array: " + exp);
+
+for(var i = 0; i < exp.length; i++){
+    
+}
+//  console.log("Total: "+sum) ;
