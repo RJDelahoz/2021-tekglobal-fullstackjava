0f79098980f665e6c7f238923214992f1ae0af5c
delahozric <delahozric@gmail.com>
2021-02-10 14:23:13 -0500

    Calc 3 complete

--- week-3/ricardo_delahoz_js.numbers/script.js
+++
@@ -1,9 +0,0 @@
-var exp = "100 + 50 - 25 + 0 + 123 - 1".split(" ");
-
-// var sum = 0;
-console.log("Array: " + exp);
-
-for(var i = 0; i < exp.length; i++){
-    
-}
-//  console.log("Total: "+sum) ;
